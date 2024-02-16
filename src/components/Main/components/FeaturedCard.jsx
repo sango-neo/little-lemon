9bb860dc3284e9d11587662b0d66cde47e1b0ed0
sango-neo <masangoneo@gmail.com>
2024-02-16 17:05:41 +0200

    styles and resp'ness part 2 - featured section

--- src/components/Main/components/FeaturedCard.jsx
+++ src/components/Main/components/FeaturedCard.jsx
@@ -4,11 +4,13 @@ import React from 'react'
 const FeaturedCard = ({ imgUrl, title, price, descr}) => {
   return (
     <article className='featured-card'>
-            <img src={imgUrl} alt="image of dish" />
+            <div className="img-box">
+              <img src={imgUrl} alt="image of dish" />
+            </div>
             <div className="card-details">
               <h3>{title}</h3>
-              <p className='food=description'>{descr}</p>
-              <p className="price">{price}</p>
+              <p className='food-description'>{descr}</p>
+              <p className="price">${(Math.round(price * 100) / 100).toFixed(2)}</p>
               <a href="#">
                 Order a delivery <span><i className="scooter-icon">{`->`}</i></span>
               </a>
